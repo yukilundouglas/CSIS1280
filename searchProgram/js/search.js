b95f53fd1d2156e23c601bf28e9bb60fa48a6741
yukilun <102889935+yukilun@users.noreply.github.com>
2022-04-04 15:50:38 -0700

    first commit

---
+++ searchProgram/js/search.js
@@ -0,0 +1,24 @@
+function search(){
+
+    var searchItem = document.getElementById("txtlookfor").value;
+    var site = 'wikipedia.org';
+    
+    if(searchItem!=""){
+        var engine = document.getElementById("dropdown").value;
+
+        if(engine == "1"){
+            var query = "http://www.bing.com/search?q="+ encodeURIComponent(searchItem) + " site:"+ site;
+        }
+        else{
+            var query = "http://www.google.com/search?q="+ encodeURIComponent(searchItem) + " site:"+ site;
+        }
+        
+        location.href = query;
+
+    }
+    else{
+        alert("Please type the word or words for which you want to search.");
+    }
+
+
+}
